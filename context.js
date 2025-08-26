02764bd617fc1c145ac4f81134010caaa13fdd9b
domainexpansion <fahimmuntasironi2002@gmail.com>
2025-08-27 04:21:45 +0600

    heart counter added

---
+++ context.js
@@ -0,0 +1,26 @@
+let heart_count = 0;
+let coin_count = 100;
+let copy_count = 0;
+
+document.getElementById("coin").innerHTML = coin_count;
+document.getElementById("copy").innerHTML = copy_count + " Copy";
+
+// heart count increase
+
+const favourites = document.querySelectorAll(".favourite");
+favourites.forEach((fav) => {
+  let fav_state = false;
+  fav.addEventListener("click", () => {
+    if (fav_state === false) {
+      heart_count ++;
+
+      document.getElementById("heart").innerHTML = heart_count;
+      fav_state = true;
+    } else {
+      heart_count--;
+
+      document.getElementById("heart").innerHTML = heart_count;
+      fav_state = false;
+    }
+  });
+});
